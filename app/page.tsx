a1449f13081114fcb2cc8ecd73ebae88eb9080b0
Gregory Vicent <gregoryvicent.dev@gmail.com>
2023-11-04 13:46:55 -0400

    Finished home section

--- app/page.tsx
+++ app/page.tsx
@@ -12,28 +12,33 @@ import imageLogo from '../public/images/logogregory.png'
 
 export default function Home() {
   return (
-    <main className="h-screen">
-      <section className="grid grid-cols-1 h-screen py-16 px-5 min-[500px]:px-20 sm:px-40 md:px-10 md:grid-cols-2 md:grid-rows-2 md:pt-1">
-        <div className="self-start md:row-start-2 md:row-end-4 md:col-start-3 md:col-end-4 md:mx-10">
-          <Image
-            className="rounded-full w-40 mx-auto md:w-56"
-            src={imageMe}
-            alt="Gregory Vicent"
-          />
+    <main className="h-screen grid grid-rows-2">
+      <section className="grid grid-cols-1 h-screen py-16 px-5 min-[500px]:px-20 sm:px-40 md:grid-rows-4 md:px-8 md:py-3">
+        <div className="grid md:grid-cols-2 md:grid-rows-1 md:row-start-2 md:row-end-4 md:w-10/12 md:justify-self-center lg:w-9/12 xl:w-8/12 ">
+          <div className="self-start md:col-start-2 md:col-end-3 md:flex md:justify-end md:mt-10 ">
+            <Image
+              className="rounded-full w-40 mx-auto md:w-64 md:mx-0 lg:w-72"
+              src={imageMe}
+              alt="Gregory Vicent"
+            />
+          </div>
+          <div className="self-center mx-2 md:col-start-1 md:col-end-2 md:-rotate-12">
+            <p className="mt-10 text-center text-lg font-medium md:mt-0 md:text-3xl lg:text-4xl">Hola, soy tu programador web de confianza...</p>
+            <h1 className="animate-bounce text-center text-3xl font-bold text-blue-400 md:text-4xl md:mt-3 lg:text-6xl">Gregory Vicent</h1>
+            <Image
+              className="rounded-full w-16 mx-auto my-5"
+              src={imageLogo}
+              alt="Logo Gregory Vicent"
+            />
+          </div>
         </div>
-        <div className="self-center mx-2 md:row-start-2 md:row-end-4 md:col-start-1 md:col-end-3 md:self-start md:-rotate-12 md:mx-10">
-          <p className="mt-10 text-center text-lg font-medium md:mt-0 md:text-3xl">Hola, soy tu programador web de confianza...</p>
-          <h1 className="text-center text-3xl font-bold text-blue-400 md:text-4xl md:mt-3">Gregory Vicent</h1>
-          <Image
-            className="rounded-full w-16 mx-auto my-5"
-            src={imageLogo}
-            alt="Logo Gregory Vicent"
-          />
-        </div>
-        <div className="self-end md:row-start-1 md:row-end-2 md:col-start-1 md:col-span-4 md:self-start">
+        <div className="self-end md:row-start-1 md:row-end-2 md:self-start">
           <Header />
         </div>
       </section>
+      <footer className="self-end p-3">
+        <p className="text-center font-light">✨ Creemos algo increible juntos ✨</p>
+      </footer>
     </main>
   )
 }
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -12,34 +12,34 @@ import {
 
 const Header = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 md:mx-auto">
-      <nav>
-        <ul className="flex justify-around my-7 md:pt-3">
-          <li><Link className="font-light text-blue-400 transition-all duration-300 ease-in-out hover:font-bold md:text-lg" href="/">Trabajemos</Link></li>
-          <li><Link className="font-light text-blue-400 transition-all duration-300 ease-in-out hover:font-bold md:text-lg" href="/">Proyectos</Link></li>
-          <li><Link className="font-light text-blue-400 transition-all duration-300 ease-in-out hover:font-bold md:text-lg" href="/">Sobre mi</Link></li>
+    <div className="grid grid-cols-1 md:grid-cols-2 md:mx-auto lg:px-10 xl:px-20">
+      <nav className="mb-5 md:mb-0">
+        <ul className="flex justify-around my-7 md:pt-3 lg:justify-evenly">
+          <li><Link className="font-light text-blue-400 p-3 rounded-md shadow-black shadow-lg transition-all duration-300 ease-in-out hover:shadow-blue-300 md:text-lg" href="/">Trabajemos</Link></li>
+          <li><Link className="font-light text-blue-400 p-3 rounded-md shadow-black shadow-lg transition-all duration-300 ease-in-out hover:shadow-blue-300 md:text-lg" href="/">Proyectos</Link></li>
+          <li><Link className="font-light text-blue-400 p-3 rounded-md shadow-black shadow-lg transition-all duration-300 ease-in-out hover:shadow-blue-300 md:text-lg" href="/">Sobre mi</Link></li>
         </ul>
       </nav>
       <div className="md:mt-10 md:justify-self-end">
         <ul className="flex justify-evenly">
           <li className="md:mx-5">
             <a href="https://twitter.com/Gregory_Vicent" rel="noopener noreferrer" target="_blank">
-              <FontAwesomeIcon className="w-8 text-white transition-colors duration-300 ease-in-out hover:text-blue-500 md:w-6" icon={faTwitter} />
+              <FontAwesomeIcon className="w-8 text-white transition-colors duration-300 ease-in-out hover:text-blue-500 md:w-6 lg:w-9" icon={faTwitter} />
             </a>
           </li>
           <li className="md:mx-5">
             <a href="https://github.com/gregoryvicent" rel="noopener noreferrer" target="_blank">
-              <FontAwesomeIcon className="w-8 text-white transition-colors duration-300 ease-in-out hover:text-blue-500 md:w-6" icon={faGithub} />
+              <FontAwesomeIcon className="w-8 text-white transition-colors duration-300 ease-in-out hover:text-blue-500 md:w-6 lg:w-9" icon={faGithub} />
             </a>
           </li>
           <li className="md:mx-5">
             <a href="https://www.linkedin.com/in/gregory-vicent-dev/" rel="noopener noreferrer" target="_blank">
-              <FontAwesomeIcon className="w-8 text-white transition-colors duration-300 ease-in-out hover:text-blue-500 md:w-6" icon={faLinkedin} />
+              <FontAwesomeIcon className="w-8 text-white transition-colors duration-300 ease-in-out hover:text-blue-500 md:w-6 lg:w-9" icon={faLinkedin} />
             </a>
           </li>
           <li className="md:mx-5">
             <a href="https://www.youtube.com/channel/UCSEdxq0M1N_NpoFvbdWOjkg" rel="noopener noreferrer" target="_blank">
-              <FontAwesomeIcon className="w-8 text-white transition-colors duration-300 ease-in-out hover:text-blue-500 md:w-7" icon={faYoutube} />
+              <FontAwesomeIcon className="w-8 text-white transition-colors duration-300 ease-in-out hover:text-blue-500 md:w-6 lg:w-9" icon={faYoutube} />
             </a>
           </li>
         </ul>
